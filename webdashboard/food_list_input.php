a924054fae83e65964af77b524d9b35ae921b817
alfa2beta3 <leexingyang180@gmail.com>
2023-05-26 20:30:27 +0800

    webdashboard/food_list_input.php

---
+++ webdashboard/food_list_input.php
@@ -0,0 +1,21 @@
+<?php 
+
+$foodname = array(
+'0' => 'Pizza',
+'1' => 'Burger',
+'2' => 'Pasta',
+'3' => 'Fries', 
+'4' => 'Sandwich',
+'5' => 'Apple',
+'6' => 'Pineapple',
+'7' => 'Banana',);
+$foodimages = array(
+'0' => 'foodimages/pizza.jpg',
+'1' => 'foodimages/burger.jpg',
+'2' => 'foodimages/pasta.jpg',
+'3' => 'foodimages/fries.jpg',
+'4' => 'foodimages/sandwich.jpg',
+'5' => 'foodimages/apple.jpg',
+'6' => 'foodimages/pineapple.jpg',
+'7' => 'foodimages/banana.jpg',);
+ ?>
