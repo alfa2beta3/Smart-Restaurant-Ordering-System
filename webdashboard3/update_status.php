7356866008814ee39ecce6e31067f894345eec96
alfa2beta3 <leexingyang180@gmail.com>
2023-06-03 23:14:09 +0800

    added update status.php

---
+++ webdashboard3/update_status.php
@@ -0,0 +1,25 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "rms2") or die("Connection Failed");
+
+if ($conn) {
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $id = $_POST["id"];
+        $sts = $_POST["sts"];
+        $sts = ($sts === "0") ? "1" : "0";
+        echo 'sts is' . $sts;
+
+        $query = "UPDATE `orders` SET `status` = '$sts' WHERE `id` = $id";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "Success";
+        } else {
+            echo "Error occurred while updating status.";
+        }
+    } else {
+        echo "Invalid request.";
+    }
+} else {
+    echo "Connection Failed";
+}
+?>
